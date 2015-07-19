85e990640cfd949173a9c30ea1a9584d5709f6f1
Daiwei <daiweilu123@gmail.com>
2015-07-19 14:00:29 -0400

    Move codes

--- Avetuc/TwitterApiService.swift
+++ Avetuc/TwitterApiService.swift
@@ -99,30 +99,3 @@ class TwitterApiService {
     }
 
 }
-
-func getHomeTimeline(twitterApi: TwitterApi, since_id: String?, max_id: String?) -> StatusesHomeTimelineTask {
-    let params: [TwitterApiParam] = [.Count(200), .TrimUser(true), .ExcludeReplies(true), .IncludeEntities(true)]
-    return executeGetHomeTimeline(twitterApi, since_id, nil, params, [])
-}
-
-func executeGetHomeTimeline(
-    twitterApi: TwitterApi,
-    since_id: String?,
-    max_id: String?,
-    params: [TwitterApiParam],
-    result: [TweetData]
-) -> StatusesHomeTimelineTask
-{
-    let newParams = max_id == nil ? params : params + [.MaxId(max_id!)]
-
-    return twitterApi.statusesHomeTimeline(newParams)
-        .success { (data: [TweetData]) -> StatusesHomeTimelineTask in
-
-            if since_id != nil && data.count > 0 {
-                let oldest_tweet_id = data.last!.id_str
-                return executeGetHomeTimeline(twitterApi, since_id, String(oldest_tweet_id.toInt()! - 1), params, result + data)
-            } else {
-                return StatusesHomeTimelineTask(value: result + data)
-            }
-    }
-}
