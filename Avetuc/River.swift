786b0f0959192f1b43538543fe4aac9d49c41897
Daiwei <daiweilu123@gmail.com>
2015-08-06 14:00:05 -0400

    Fetch friends

--- Avetuc/River.swift
+++ Avetuc/River.swift
@@ -31,47 +31,20 @@ class River {
             }
             >- variable
 
-//        self.action_update
-//            >- flatMap { () -> Observable<Account> in
-//                let accounts = Array(Realm().objects(AccountModel)).map { $0.toData() }
-//                return from(accounts)
-//            }
-//            >- flatMap { (account: Account) -> Observable<Void> in
-//                requestFriendIdsStream(account)
-//                    >- doOnNext { ids in
-//                        let realm = Realm()
-//                        let accountModel = realm.objects(AccountModel).filter("user_id = %@", account.user_id).first!
-//                        for friend in accountModel.friends {
-//                            if !contains(ids, friend.id_str) {
-//                                realm.write {
-//                                    realm.delete(friend)
-//                                }
-//                            }
-//                        }
-//                    }
-//                    >- flatMap { (ids: [String]) -> Observable<[String]> in
-//                        var batch = [[String]]()
-//                        var i = 0
-//
-//                        while i < ids.count {
-//                            let end = min(i + 100, ids.count)
-//                            batch.append(Array(ids[i..<end]))
-//                            i += 100
-//                        }
-//
-//                        return from(batch)
-//                    }
-//                    >- flatMap { (ids: [String]) -> Observable<UserApiData> in
-//
-//                    }
-//
-//                return empty()
-//            }
+        let s = createUpdateAccountStream(self.action_updateAccount)
+        s.connect()
+
+        self.stream_account
+            >- subscribeNext {
+                if let account = $0 {
+                    sendNext(self.action_updateAccount, account)
+                }
+            }
     }
 
     let action_addAccountFromWeb = PublishSubject<Void>()
     let action_handleOauthCallback = PublishSubject<NSURL>()
-    let action_update = Variable<Void>()
+    let action_updateAccount = PublishSubject<Account>()
 
     let stream_addAccountError = PublishSubject<NSError>()
     let stream_account: ConnectableObservableType<Account?>
