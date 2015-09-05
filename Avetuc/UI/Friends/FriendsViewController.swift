611af208b3e3fa5c29273a80ad8118fe0e039842
Daiwei <daiweilu123@gmail.com>
2015-09-05 08:45:15 -0400

    Refactor friends table height calc

--- Avetuc/UI/Friends/FriendsViewController.swift
+++ Avetuc/UI/Friends/FriendsViewController.swift
@@ -13,6 +13,7 @@ class FriendsViewController: UITableViewController {
 
         self.tableView.registerClass(FriendTableCell.self, forCellReuseIdentifier: CELL_IDENTIFIER)
         self.tableView.layoutMargins = UIEdgeInsetsZero
+        self.tableView.rowHeight = FriendTableCell.heightForContent()
 
         self.tableView.emptyDataSetSource = self
         self.tableView.emptyDataSetDelegate = self
@@ -37,51 +38,8 @@ class FriendsViewController: UITableViewController {
                 self.refreshControl!.endRefreshing()
                 self.friends = friends
                 self.tableView.reloadData()
-//                self.reloadTable(diffResult)
             }
     }
-
-//    func reloadTable(diffResult: DiffResult<User>)
-//    {
-//        switch diffResult {
-//        case .Initial(let friends):
-//            #if DEBUG
-//                println("friends table initial load")
-//            #endif
-//
-//            self.tableView.reloadData()
-//        case .Differences(let differences):
-//            #if DEBUG
-//                println("friends table diff load")
-//                println(differences)
-//            #endif
-//
-//            self.tableView.beginUpdates()
-//
-//            let insertIndexPaths = differences.insert.map { NSIndexPath(forRow: $0.index, inSection: 0) }
-//            self.tableView.insertRowsAtIndexPaths(insertIndexPaths, withRowAnimation: .Bottom)
-//
-//            let removeIndexPaths = differences.remove.map { NSIndexPath(forRow: $0.index, inSection: 0) }
-//            self.tableView.deleteRowsAtIndexPaths(removeIndexPaths, withRowAnimation: .Bottom)
-//
-//            let updateIndexPaths = differences.update.map { NSIndexPath(forRow: $0.index, inSection: 0) }
-//            self.tableView.reloadRowsAtIndexPaths(updateIndexPaths, withRowAnimation: .Fade)
-//
-//            for item in differences.move {
-//                let indexPath = NSIndexPath(forRow: item.oldIndex, inSection: 0)
-//
-//                if let cell = self.tableView.cellForRowAtIndexPath(indexPath) as? FriendTableCell {
-//                    cell.refresh()
-//                }
-//
-//                self.tableView.moveRowAtIndexPath(
-//                    indexPath,
-//                    toIndexPath: NSIndexPath(forRow: item.newIndex, inSection: 0))
-//            }
-//
-//            self.tableView.endUpdates()
-//        }
-//    }
 
     // MARK: - No use
 
